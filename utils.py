806e0214b4ff8080d97d8135a96cfd8b100064e0
Lizbew Chou <my12time@gmail.com>
2013-09-28 22:39:56 +0800

    fix code highlight of python-markdown
    Use python-markdown's extension codehilite

--- utils.py
+++ utils.py
@@ -4,5 +4,5 @@ Some utils function here
 import markdown
 
 def convert_markdown_text(md_text):
-    return markdown.markdown(md_text)
+    return markdown.markdown(md_text, extensions=['extra', 'codehilite(linenums=False,css_class=prettyprint)'])
 
